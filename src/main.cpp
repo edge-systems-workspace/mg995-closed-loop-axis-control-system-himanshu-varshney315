26dec26c2d6fb7f594f6a64a84f459c219ce669c
himanshu-varshney315 <himanshu.gupta_cs.aiml24@gla.acin>
2026-02-19 09:17:11 +0530

    all done

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
 #include <Arduino.h>
+#include <Servo.h>
+Servo myServo;
+int trigPin=9;
+int echoPin=10;
+
 void setup() {
 // write your initialization code here
+    pinMode(trigPin,OUTPUT);
+    pinMode(echoPin,INPUT);
+    Serial.begin(9600);
+    myServo.attach(300);
+
 }
 
 
